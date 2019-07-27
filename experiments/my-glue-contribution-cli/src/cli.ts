dfb9179648ecb9feccbdfc5adf103378957cec7e
Alex Korzhikov <korzio@gmail.com>
2019-07-27 17:35:07 +0200

    feat: 🎸 add gluegun based cli experiment
    BREAKING CHANGE: n
    
    Closes: 1

---
+++ experiments/my-glue-contribution-cli/src/cli.ts
@@ -0,0 +1,26 @@
+const { build } = require('gluegun')
+
+/**
+ * Create the cli and kick it off
+ */
+async function run(argv) {
+  // create a CLI runtime
+  const cli = build()
+    .brand('my-glue-contribution-cli')
+    .src(__dirname)
+    .plugins('./node_modules', {
+      matching: 'my-glue-contribution-cli-*',
+      hidden: true
+    })
+    .help() // provides default for help, h, --help, -h
+    .version() // provides default for version, v, --version, -v
+    .create()
+
+  // and run it
+  const toolbox = await cli.run(argv)
+
+  // send it back (for testing, mostly)
+  return toolbox
+}
+
+module.exports = { run }
