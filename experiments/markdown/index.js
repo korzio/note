8d78a90e787e540957ede22d1e292316c871826f
Alex Korzhikov <korzio@gmail.com>
2019-09-01 16:07:42 +0200

    feat: 🎸 Implement meeting command
    Also move events processing to markdown index
    
    BREAKING CHANGE: n
    
    Closes: 29

--- experiments/markdown/index.js
+++ experiments/markdown/index.js
@@ -1,4 +1,6 @@
-const { readFileSync } = require('fs')
+const EventEmitter = require('events')
+const { readFileSync, writeFileSync } = require('fs')
+const { basename } = require('path')
 
 const unified = require('unified')
 const markdown = require('remark-parse')
@@ -6,12 +8,52 @@ const stringify = require('remark-stringify')
 
 const visit = require('./visit')
 
-unified()
-  .use(markdown)
-  .use(visit)
-  .use(stringify)
-  .process(readFileSync(`${__dirname}/test.md`), function(err, file) {
-    if (err) throw err
-    console.log(file)
-  })
+module.exports = (filePath, process) => {
+  const file = readFileSync(filePath)
+
+  async function* inOrderPrompts(prompts) {
+    let done
+    for (const prompt of prompts) {
+      done = await process(...prompt)
+      yield done
+    }
+  }
   
+  async function iteratePrompts(prompts) {
+    let doneOuter
+    for await (let done of inOrderPrompts(prompts)) {
+      doneOuter = done
+    }
+  
+    doneOuter()
+  }  
+
+  const events = new EventEmitter()
+  events.on('prompt', planNext)
+
+  // TODO refactor for events to iterators without timeout
+  const promptsQueue = []
+  let timeout
+  function planNext() {
+    promptsQueue.push([...arguments])
+
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      iteratePrompts(promptsQueue)
+    }, 1e3)
+  }
+
+  unified()
+    .use(markdown)
+    .use(visit(events))
+    .use(stringify)
+    .process(file, function(err, file) {
+      if (err) throw err
+
+      console.log(`Updating ${basename(filePath)} ...`)
+      writeFileSync(filePath, String(file))
+
+      console.log(`Finished`)
+    })
+}
+
--- experiments/markdown/visit.js
+++ experiments/markdown/visit.js
@@ -1,82 +1,16 @@
 // aka command utils
-const EventEmitter = require('events')
 const visit = require('unist-util-visit')
 
-module.exports = () => (tree) => new Promise((resolve) => {
-  const events = new EventEmitter()
-  let timeout
-  const planResolve = () => {
-    timeout && timeout.unref()
-    timeout = setTimeout(resolve, 1000)
-  }
-  
-  events.on('inquirer', ({ value: text }, node) => {
-    console.log(arguments)
-    console.log(`${text} \n${node}`)
-    // modify nodes
-    setTimeout(() => { 
-      node.checked = true
-      planResolve()
-    }, 500)
-  })
-
+module.exports = (events) => () => (tree) => new Promise((resolve) => {
   // TODO promisify
   visit(tree, 'list', node => {
     visit(node, 'listItem', (node) => {
       visit(node, 'text', (textNode) => {
-        const text = textNode.value ? textNode.value.trim() : ''
         // TODO comments to log
         // processing text
-        events.emit('inquirer', textNode, node)
+        events.emit('prompt', textNode, node, resolve)
         // TODO does it show a syncronous / asyncronous prompt functionality?
       })
     })
   })
 })
-
-// module.exports = () => (tree, file, nextOut) => {
-// visit(tree, 'list', node => {
-//   visit(node, 'listItem', function (node) {
-//     module.exports.next = next
-
-//     visit(node, 'text', function (textNodeIn) {
-//       const text = textNode.value ? textNode.value.trim() : ''
-//     })
-//   })
-// })
-
-//   return new Promise(() => {})
-// }
-
-// async function get(n) {
-//   return new Promise(r => setTimeout(() => r(n), n * 100))
-// }
-
-// async function* asyncGenerator(arr) {
-//   let anyResolve
-
-//   arr.map(get).forEach(async request => {
-//     const anyResult = await request
-//     anyResolve(anyResult)
-//   })
-
-//   while(true) {
-//     yield await new Promise((resolve) => {
-//       anyResolve = resolve
-//     })
-//   }
-// }
-
-// async function* inOrderRequests(urls) {
-//   for (const url of urls) {
-//     const response = await fetch(url)
-//     const text = await response.text()
-//     yield text
-//   }
-// }
-
-// (async function() {
-//   for await (let num of asyncGenerator([4, 3, 2, 1, 0])) {
-//     console.log(num)
-//   }
-// })()
