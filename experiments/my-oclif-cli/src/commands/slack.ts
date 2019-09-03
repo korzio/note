091e75c4f61dd4609f62a4cec62466a90c58ecb1
Pavlik Kiselev <paulcodiny@gmail.com>
2019-09-04 00:13:41 +0200

    Add questions about params

--- experiments/my-oclif-cli/src/commands/slack.ts
+++ experiments/my-oclif-cli/src/commands/slack.ts
@@ -1,10 +1,14 @@
 import {readFileSync} from 'fs';
 import {Command, flags} from '@oclif/command'
 import {IncomingWebhook} from '@slack/webhook'
+import cli from 'cli-ux'
 import Handlebars from 'handlebars';
 
 import {createEnvironmentFlags} from '../utils'
 
+interface TemplateParamDefinition {
+  [key: string]: string
+}
 
 const slackEnvironmentVariables = createEnvironmentFlags([
   ['slack_webhook_url', 'SLACK_WEBHOOK_URL'],
@@ -41,17 +45,28 @@ export default class Slack extends Command {
     const template = readFileSync('./src/templates/slack.html.hbs').toString('utf8')
 
     const templateAst = Handlebars.parse(template)
-    const params = []
-    templateAst.body.forEach(statement => {
+    const params: TemplateParamDefinition = {}
+    await templateAst.body.reduce((p: Promise<string>, statement) => {
       if (statement.type === 'MustacheStatement') {
-        params.push(statement.path.original)
+        return p
+          .then(() => cli.prompt(`What is the value of "${statement.path.original}"`))
+          .then((value: string) => params[statement.path.original] = value)
+      } else {
+        return p;
       }
-    })
-    console.log(params)
+    }, Promise.resolve());
 
-    const webhook = new IncomingWebhook(flags.slack_webhook_url as string);
-    await webhook.send({
-      text: 'I\'ve got news for you...',
-    });
+    const source = Handlebars.compile(template);
+
+    console.log('- The following message will be sent: -');
+    console.log(source(params));
+    const needToContinue = await cli.prompt('Continue? Yes/no', { required: false, default: 'yes' });
+    const continueOptions = ['y', 'yes'];
+    if (continueOptions.includes(needToContinue.toLowerCase())) {
+      const webhook = new IncomingWebhook(flags.slack_webhook_url as string);
+      await webhook.send(source(params));
+
+      console.log('The message has been sent successfully!');
+    }
   }
 }
