a5ff0b017500c68b814d89238287c8ae3cb8f19a
Pavlik Kiselev <paulcodiny@gmail.com>
2019-09-24 08:54:56 +0200

    WIP: added generated types and updated issues to use GraphQL API

--- experiments/my-oclif-cli/src/commands/gh/issues.ts
+++ experiments/my-oclif-cli/src/commands/gh/issues.ts
@@ -1,12 +1,26 @@
 import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
 import chalk from 'chalk'
+import { graphql } from '@octokit/graphql'
+
+import { Issue } from './types';
+
 import Octokit = require('@octokit/rest')
+
+
 import { createEnvironmentFlags } from '../../utils';
+import { GraphQlQueryResponseData } from '@octokit/graphql/dist-types/types';
+import object from 'cli-ux/lib/styled/object';
 
 const ghIssuesEnvironmentVariables = createEnvironmentFlags([
   ['github_personal_token', 'GITHUB_PERSONAL_TOKEN'],
 ])
+
+type Repository = GraphQlQueryResponseData & {
+  issues: {
+    edges: Array<Issue>
+  }
+}
 
 export default class GhIssues extends Command {
   static description = 'Get the list of the issues of korzio/note repo'
@@ -41,11 +55,44 @@ export default class GhIssues extends Command {
       auth: flags.github_personal_token as string,
     });
 
-    // https://github.com/octokit/graphql.js can be used
-    const { data: issues } = await octokit.issues.listForRepo({
-      owner: args.owner,
-      repo: args.repo,
+    // const { data: issues } = await octokit.issues.listForRepo({
+    //   owner: args.owner,
+    //   repo: args.repo,
+    // });
+
+    const graphqlWithAuth = graphql.defaults({
+      headers: {
+        authorization: `token ${flags.github_personal_token as string}`
+      }
     });
+    const { repository: { issues: { edges: issueNodes } } } = await graphqlWithAuth(`
+      {
+        repository(owner: "${args.owner}", name: "${args.repo}") {
+          issues(last: 10) {
+            edges {
+              node {
+                title
+                assignees(first: 1) {
+                  edges {
+                    node {
+                      login  
+                    }
+                  }
+                }
+                state
+                url
+              }
+            }
+          }
+        }
+      }
+    `) as { repository: Repository };
+
+    const issues = issueNodes.map(({ node }) => ({ ...node }))
+    // console.log(issues[0].assignee)
+    // console.log(issues[0].assignees.edges[0].node.login)
+    //
+    // return
 
     cli.action.stop()
 
@@ -54,7 +101,7 @@ export default class GhIssues extends Command {
 
       },
       assignee: {
-        get: row => row.assignee ? row.assignee.login : null,
+        get: row => row.assignees.edges.length ? row.assignees.edges[0].node.login : null,
       },
       state: {
         get: row => row.state === 'open' ? chalk.green('open') : chalk.red('closed'),
