a7e3c22b27feb78fd51bba04abf474a9a94cd255
Pavlik Kiselev <paulcodiny@gmail.com>
2019-11-09 21:06:56 +0100

    feat: Added tests for issues command

--- experiments/my-oclif-cli/test/commands/gh/issues.test.ts
+++ experiments/my-oclif-cli/test/commands/gh/issues.test.ts
@@ -2,28 +2,30 @@ import {expect, test} from '@oclif/test'
 
 describe('gh:issues', () => {
   test
-    .stdout()
-    .command(['gh:issues'])
     .nock('https://api.github.com', api => api
       .get('/repos/korzio/note/issues')
       // user is logged in, return their name
-      .reply(200, {
-        data: [
-          {
-            number: 1,
-            title: 'Hello',
-            assignee: {
-              login: 'test'
-            },
-            state: 'open',
-            html_url: 'https://github.com/'
-          }
-        ]
-      })
+      .reply(200, [
+        {
+          number: 'Number:1',
+          title: 'Title:Hello',
+          assignee: {
+            login: 'Assignee.login:test'
+          },
+          state: 'open',
+          html_url: 'HtmlUrl:https://github.com/'
+        }
+      ])
     )
-
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
+    .stdout({print: true})
+    .command(['gh:issues'])
+    .it('should format the table', ctx => {
+      expect(ctx.stdout)
+        .to.contain('Number:1')
+        .and.to.contain('Title:Hello')
+        .and.to.contain('Assignee.login:test')
+        .and.to.contain('open')
+        .and.to.contain('HtmlUrl:https://github.com/')
     })
 
   // test
